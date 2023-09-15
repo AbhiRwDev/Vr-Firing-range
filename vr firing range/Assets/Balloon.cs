29b305697485940efdf6b57437d5e65967949f2c
anshurw <anshurw@gmail.com>
2023-09-16 01:16:54 +0530

    update

--- vr firing range/Assets/Balloon.cs
+++ vr firing range/Assets/Balloon.cs
@@ -7,9 +7,18 @@ public class Balloon : MonoBehaviour
     public int number=0;
     public TextMeshPro Ntext;
     private BaloonController Bcontroller;
+
+    public float moveDistance = 0.5f;  // The distance to move left and right
+    private float startpos = 1.0f;    // The speed of the movement
+    private float endpos = 1.0f;    // The speed of the movement
+    
+    public float movespeed;
+    private bool isMovingRight = true;
     // Start is called before the first frame update
     void Start()
     {
+        startpos = transform.position.x-moveDistance;
+        endpos = transform.position.x+ moveDistance;
         Bcontroller = FindAnyObjectByType<BaloonController>();
     }
 
@@ -17,7 +26,31 @@ public class Balloon : MonoBehaviour
     void Update()
     {
         Ntext.text = number.ToString();   
+        switch(Bcontroller.Level)
+        {
+            case 1:
+
+                break;
+            case 2:
+               
+                moveDistance = 3;
+                MoveLeftAndRight();
+                break;
+            
+        }
     }
+    void MoveLeftAndRight()
+    {
+        if(transform.position.x<=startpos)
+        {
+            movespeed = -movespeed;
+        }else if(transform.position.x >= startpos + moveDistance)
+        {
+            movespeed *= -1;
+        }
+        transform.localPosition += Vector3.right * movespeed * Time.deltaTime;
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.collider.CompareTag("Bullet"))
